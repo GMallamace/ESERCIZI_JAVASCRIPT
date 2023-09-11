8a4cb6e52f27087600cd9582737b0bd400166605
Guido Mallamace <guidomallamace97@gmail.com>
2023-09-11 17:45:17 +0200

    Aggiornamento

---
+++ EserciziJavascript.js
@@ -0,0 +1,44 @@
+/*
+  Somma e media
+  Scrivi un programma che dati 5 numeri restituisca in output la somma e la media.
+
+  Esempio:
+    Input: a = 1, b = 2, c = 3, d = 4, e = 5
+    Output: somma = 15, media = 3
+*/
+
+
+
+
+/*
+  Quanti anni ha?
+  Scrivi un programma che dato l'anno corrente e un anno di nascita determini:
+    - l'età della persona,
+    - quanti anni sono necessari per raggiungere i 100
+  Restituisca in output entrambi i risultati.
+
+  Esempio:
+    Input: anno corrente = 2018, anno di nascita = 1991
+    Ouput: età = 27, anni mancanti = 73
+*/
+
+
+
+
+/*
+  Gatti in fila
+  Scrivi un programma che dato:
+    a) Un numero totale di gatti
+    b) Il numero dei gatti presenti in ogni fila
+  Restituisca in output:
+    - Il numero di file risultanti
+    - Nel caso l'ultima fila risulti incompleta indicare il numero di gatti mancanti per completarla (quanti gatti mancano per arrivare a b?)
+
+  Esempio:
+    Input: numero di gatti = 44, gatti per fila = 6
+    Output: numero di file = 8, gatti mancanti = 4
+
+  Consigli:
+  Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
+  utilizza Math.ceil (arrotonda sempre all'intero superiore).
+*/
